da2a907a6392b46e678a7d9a23fe351c7b64e948
LLetiziaxu <115707749+LLetiziaxu@users.noreply.github.com>
2022-10-20 12:59:09 +0800

    Add files via upload

---
+++ Classes/SHVFS_P101_GD08_C1/SHVFS_P101_GD08_Letizia/SHVFS_P101_GD08_Letizia/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+#region Structure of a program
+Console.WriteLine("Hello, World!");
+namespace SHVFS_P101_GD08_Letizia
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
+#endregion
+
