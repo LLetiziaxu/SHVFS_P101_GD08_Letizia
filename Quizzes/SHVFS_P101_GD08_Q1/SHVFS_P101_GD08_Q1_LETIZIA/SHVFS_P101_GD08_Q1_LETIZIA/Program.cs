4fc29b48cda20c813843e189e160a63c3e13716c
LLetiziaxu <115707749+LLetiziaxu@users.noreply.github.com>
2022-10-20 13:08:34 +0800

    Add files via upload

---
+++ Quizzes/SHVFS_P101_GD08_Q1/SHVFS_P101_GD08_Q1_LETIZIA/SHVFS_P101_GD08_Q1_LETIZIA/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+#region Structure of a program
+
+namespace SHVFS_P101_GD08_Q1_LETIZIA
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine($"My name is Letizia! Chris, you are AWESOME!");
+            Console.ReadLine();
+
+        }
+    }
+}
+#endregion
