a8462407417580f2e2113b0c969b9d655e0fef12
LLetiziaxu <letiziaxu@qq.com>
2022-11-10 22:38:36 +0800

    update 1

--- Quizzes/SHVFS_P101_GD08_Q2022-11-10_Letizia/SHVFS_P101_GD08_Q2022-11-10_Letizia/SHVFS_P101_GD08_Q2022-11-10_Letizia/Program.cs
+++ Quizzes/SHVFS_P101_GD08_Q2022-11-10_Letizia/SHVFS_P101_GD08_Q2022-11-10_Letizia/SHVFS_P101_GD08_Q2022-11-10_Letizia/Program.cs
@@ -9,13 +9,31 @@ namespace SHVFS_P101_GD08_Letizia
       
         public static void Main(string[] args)
         {
-            string name, Apple, Peach, Lemon, Melon, Grape;
-            string words = new string[4] { Apple, Peach, Lemon, Melon };  
+            int time = 0;
+            string name;
+            string[] words = new string[] {"Apple","Peach","Lemon","Melon"};  
+            Random random = new Random();
+            int i=random.Next(words.Length);
             Console.Clear();
             Console.WriteLine($"Hello, Welocome to the game, may I have your name");
             name = Console.ReadLine();
             Console.Clear();
             Console.WriteLine($"Oh hello {name} Come and play the guessing game!");
+            string typing="";
+            while(typing != words[i])
+            {
+                Console.WriteLine($"Please guess a word from list: 'Apple','Peach','Lemon',Melon'");
+                typing = Convert.ToString(Console.ReadLine());
+                //if (typing != words[i])
+                //{
+                //    Console.WriteLine($"Unfortunately, you are wrong. Please try again.");
+                //}
+                //Console.WriteLine($"Congrats, You are write.");
+                Console.Clear();
+                time++;
+            }
+            Console.WriteLine($"Thanks you have try {time} times");
+            Console.ReadLine();
 
 
 
